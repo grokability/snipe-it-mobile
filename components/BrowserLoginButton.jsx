c03dec4a438a145dfeac456638152d2401b89cc3
spencerrlongg <spencer@spencerlong.com>
2025-09-23 00:23:16 -0500

    packages, copied gh example,changed urls etc

--- components/BrowserLoginButton.jsx
+++ components/BrowserLoginButton.jsx
@@ -1,22 +1,69 @@
-import React, { useState } from 'react';
-import { Button, Text } from 'react-native';
-import * as WebBrowser from "expo-web-browser";
+// import React, { useState } from 'react';
+// import { Button, Text } from 'react-native';
+// import * as WebBrowser from "expo-web-browser";
+//
+// const BrowserLoginButton = ({ domain }) => {
+//     const [result, setResult] = useState(null);
+//
+//     const handleBrowserOpen = async () => {
+//         console.log('open browser');
+//         let result = await WebBrowser.openAuthSessionAsync(
+//             domain + '/login?client=Snipe-IT-Mobile',
+//             'com.grokability.snipeitmobile://**'
+//         );
+//         setResult(result);
+//     };
+//
+//     return (
+//         <>
+//             <Button title="Open SnipeLogin" onPress={handleBrowserOpen} />
+//             <Text>{result && JSON.stringify(result)}</Text>
+//         </>
+//     );
+// };
+//
+// export default BrowserLoginButton;
+import { useEffect } from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { Button } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
+
+// Endpoint
+const discovery = {
+    authorizationEndpoint: domain + '/login?client=Snipe-IT-Mobile',
+    tokenEndpoint: domain + '/oauth/token',
+    // revocationEndpoint: 'https://github.com/settings/connections/applications/<CLIENT_ID>',
+};
 
 const BrowserLoginButton = ({ domain }) => {
-    const [result, setResult] = useState(null);
+    const [request, response, promptAsync] = useAuthRequest(
+        {
+            clientId: 'CLIENT_ID',
+            scopes: ['identity'],
+            redirectUri: makeRedirectUri({
+                scheme: 'com.grokability.snipeitmobile://**'
+            }),
+        },
+        discovery
+    );
 
-    const handleBrowserOpen = async () => {
-        console.log('open browser');
-        let result = await WebBrowser.openAuthSessionAsync(domain + '/login', 'app://snipe/callback');
-        setResult(result);
-    };
+    useEffect(() => {
+        if (response?.type === 'success') {
+            const { code } = response.params;
+        }
+    }, [response]);
 
     return (
-        <>
-            <Button title="Open SnipeLogin" onPress={handleBrowserOpen} />
-            <Text>{result && JSON.stringify(result)}</Text>
-        </>
+        <Button
+            disabled={!request}
+            title="Web Login"
+            onPress={() => {
+                promptAsync();
+            }}
+        />
     );
-};
+}
 
 export default BrowserLoginButton;
