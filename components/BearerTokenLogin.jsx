dbed2b3d893f04c9988f50d0a07a518b18fe0cb5
spencerrlongg <spencer@spencerlong.com>
2025-10-08 19:49:26 -0500

    start of switching

---
+++ components/BearerTokenLogin.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, TextInput, Text, TouchableOpacity, Alert } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+
+const LoginForm = ({ onLogin, onDomainChange }) => {
+    const [domain, setDomain] = useState('https://example.example.com');
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const loadDomain = async () => {
+            const savedDomain = await SecureStore.getItemAsync('domain');
+            if (savedDomain) {
+                setDomain(savedDomain);
+                if (onDomainChange) onDomainChange(savedDomain);
+            }
+        };
+        loadDomain();
+    }, []); // this doesn't have a dep, but i'm not sure how else to make it work...
+    // and i don't know why it's happening here, because it hasn't happened on the other versions of this...
+
+    useEffect(() => {
+        const savedBearerToken = SecureStore.getItemAsync('bearer_token');
+        if (savedBearerToken) {
+            setToken(savedBearerToken);
+        }
+    }, [token])
+
+    const handleDomainChange = (text) => {
+        setDomain(text);
+        if (onDomainChange) onDomainChange(text);
+    };
+
+    const handleLogin = async () => {
+        try {
+            await onLogin(username, password, domain);
+        } catch (error) {
+            Alert.alert("Login Failed", "Please check your username/password");
+        }
+    };
+
+    return (
+        <>
+            <TextInput
+                placeholder="Domain"
+                onChangeText={handleDomainChange}
+                value={domain}
+                style={styles.input}
+                placeholderTextColor="gray"
+                textContentType="URL"
+                autoCapitalize="none"
+            />
+            <TextInput
+                placeholder="Token"
+                onChangeText={setToken}
+                value={token}
+                style={styles.input}
+                placeholderTextColor="gray"
+                autoCapitalize="none"
+            />
+            <TouchableOpacity onPress={handleLogin}>
+                <Text>Login</Text>
+            </TouchableOpacity>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 12,
+        padding: 10,
+        width: '100%',
+    },
+});
+
+export default LoginForm;
