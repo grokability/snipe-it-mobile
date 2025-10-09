2103012089117fea102d5e13cc141f3d6f67645c
spencerrlongg <spencer@spencerlong.com>
2025-10-09 12:04:57 -0500

    bearer/domain working

--- context/AuthProvider.jsx
+++ context/AuthProvider.jsx
@@ -79,25 +79,28 @@ export const AuthProvider = ({children}) => {
                     });
                 },
                 logout: () => {
-                    console.log('logout');
-                    return makeRequest({
-                        url: '/mobile/logout',
-                        method: 'POST',
-                        isAuth: true,
-                        data: {
-                            token_id: user.token_id,
-                        },
-                        headers: { 'Authorization': `Bearer ${user.token}` }
-                    }).then(response => {
-                        console.log('logout response');
+                    // commenting out most of this to do a regular bearer token logout of the mobile app
+                    // this means the token is NOT invalidated when logging out for now.
+                    // console.log('logout');
+                    // return makeRequest({
+                    //     url: '/mobile/logout',
+                    //     method: 'POST',
+                    //     isAuth: true,
+                    //     data: {
+                    //         token_id: user.token_id,
+                    //     },
+                    //     headers: { 'Authorization': `Bearer ${user.token}` }
+                    // }).then(response => {
+                    //     console.log('logout response');
+                    // THIS IS ALL THAT HAPPENS AT LOGOUT
                         SecureStore.deleteItemAsync('user');
                         setUser(null);
                         setIsAuthenticated(false);
-                        console.log(response);
-                    }).catch(error => {
-                        console.log('logout error');
-                        console.error(error);
-                    });
+                    //     console.log(response);
+                    // }).catch(error => {
+                    //     console.log('logout error');
+                    //     console.error(error);
+                    // });
                 },
                 bearerLogin: (domain, token) => {
                     setIsLoading(true);
@@ -116,20 +119,27 @@ export const AuthProvider = ({children}) => {
                     })
                         .then(response => {
                             console.log(response);
-                            setUser(response);
                             setIsAuthenticated(true);
                             SecureStore.setItemAsync('domain', domain);
+                            const userResponse = {
+                                token: token,
+                                id: response.id,
+                                first_name: response.first_name,
+                                last_name: response.last_name,
+                                email: response.email,
+                            }
+                            setUser(userResponse);
                             // need to check that this is usable data, i'm imagining it should be
-                            // SecureStore.setItemAsync('user', JSON.stringify(user));
+                            SecureStore.setItemAsync('user', JSON.stringify(user, token));
                             // something here isn't working, i guess I could save it separately, but it makes sense
-                            // to have it on the user object...
-                            SecureStore.setItemAsync('user.token', token);
+                            // to have it on the user object... (and then i wouldn't have to change all the requests)
+                            // SecureStore.setItemAsync('user.token', token);
                             setIsLoading(false);
                         })
                         .catch(error => {
                             setUser(null);
                             setIsAuthenticated(false);
-                            console.error(error);
+                            console.error(error.message);
                         });
                 },
             }}
