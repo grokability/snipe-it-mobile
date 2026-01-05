405beb778eee53e2ba5f236544d5de2af73ce247
spencerrlongg <spencer@spencerlong.com>
2026-01-05 14:22:38 -0600

    font awesome icons

--- app/(authenticated)/(tabs)/_layout.jsx
+++ app/(authenticated)/(tabs)/_layout.jsx
@@ -1,50 +1,54 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {useContext} from "react";
 import {AuthContext} from "@/context/AuthProvider";
-import { DynamicColorIOS } from 'react-native';
-import {NativeTabs, Label, Icon} from "expo-router/unstable-native-tabs";
+import {DynamicColorIOS, Platform} from 'react-native';
+import {NativeTabs, Label, Icon, VectorIcon} from "expo-router/unstable-native-tabs";
 
 console.log('TAB LAYOUT MOUNTED')
 export default function TabLayout() {
     const { user } = useContext(AuthContext);
     return (
             <NativeTabs
-                // labelStyle={{
-                //     // For the text color
-                //     color: DynamicColorIOS({
-                //         dark: 'purple',
-                //         light: 'purple',
-                //     }),
-                // }}
-                // // For the selected icon color
-                // tintColor={DynamicColorIOS({
-                //     dark: 'purple',
-                //     light: 'purple',
-                // })}
+                labelStyle={{
+                    // For the text color
+                    color:
+                        Platform.OS === 'ios' &&
+                        DynamicColorIOS({
+                        dark: 'black',
+                        light: 'black',
+                    }),
+                }}
+                // For the selected icon color
+                tintColor={
+                    Platform.OS === 'ios' &&
+                    DynamicColorIOS({
+                    dark: 'purple',
+                    light: 'purple',
+                })}
             >
                 <NativeTabs.Trigger name="home">
                     <Label>Home</Label>
-                    <Icon sf="house" />
+                    <Icon src={<VectorIcon family={FontAwesome} name="home" />} />
                 </NativeTabs.Trigger>
 
                 <NativeTabs.Trigger name="(assets)">
                     <Label>Assets</Label>
-                    <Icon sf="barcode" />
+                    <Icon src={<VectorIcon family={FontAwesome} name="barcode" />} />
                 </NativeTabs.Trigger>
 
                 <NativeTabs.Trigger name="accessories">
                     <Label>Accessories</Label>
-                    <Icon sf="keyboard" />
+                    <Icon src={<VectorIcon family={FontAwesome} name="keyboard-o" />} />
                 </NativeTabs.Trigger>
 
                 <NativeTabs.Trigger name="consumables">
                     <Label>Consumables</Label>
-                    <Icon sf="cart" />
+                    <Icon src={<VectorIcon family={FontAwesome} name="tint" />} />
                 </NativeTabs.Trigger>
 
                 <NativeTabs.Trigger name="components">
                     <Label>Components</Label>
-                    <Icon sf="storefront" />
+                    <Icon src={<VectorIcon family={FontAwesome} name="hdd-o" />} />
                 </NativeTabs.Trigger>
 
                 <NativeTabs.Trigger name="search" role="search">
