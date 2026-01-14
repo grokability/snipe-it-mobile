099bbb2bbb9a4f89a9d261127610f0ad237abd8f
spencerrlongg <spencer@spencerlong.com>
2026-01-13 19:11:38 -0600

    a little cleanup

--- context/AuthProvider.jsx
+++ context/AuthProvider.jsx
@@ -37,52 +37,10 @@ export const AuthProvider = ({children}) => {
                 isAuthenticated,
                 isLoading,
                 setUser,
-                // login: (username, password, domain) => {
-                //     if(!domain) {
-                //         console.log('domain is empty');
-                //         setIsLoading(false);
-                //         return;
-                //     }
-                //     SecureStore.deleteItemAsync('user');
-                //     setUser(null);
-                //     setIsAuthenticated(false);
-                //     setIsLoading(true);
-                //     makeRequest({
-                //         domain: domain,
-                //         url: '/mobile/login',
-                //         method: 'POST',
-                //         isAuth: true,
-                //         data: {
-                //             username: username,
-                //             password: password,
-                //             device_name: deviceName,
-                //         }
-                //     }).then(response => {
-                //         console.log(response);
-                //         const userResponse = {
-                //             token: response.token,
-                //             token_id: response.token_id,
-                //             id: response.user.id,
-                //             first_name: response.user.first_name,
-                //             last_name: response.user.last_name,
-                //             email: response.user.email,
-                //         }
-                //         setUser(userResponse);
-                //         setIsAuthenticated(true);
-                //         console.log(isAuthenticated);
-                //         SecureStore.setItemAsync('domain', domain);
-                //         SecureStore.setItemAsync('user', JSON.stringify(userResponse));
-                //         setIsLoading(false);
-                //     }).catch(error => {
-                //         setUser(null);
-                //         setIsAuthenticated(false);
-                //         console.error(error);
-                //     });
-                // },
                 logout: () => {
                     // commenting out most of this to do a regular bearer token logout of the mobile app
                     // this means the token is NOT invalidated when logging out for now.
-                    // console.log('logout');
+                    console.log('logout');
                     // return makeRequest({
                     //     url: '/mobile/logout',
                     //     method: 'POST',
@@ -93,7 +51,6 @@ export const AuthProvider = ({children}) => {
                     //     headers: { 'Authorization': `Bearer ${user.token}` }
                     // }).then(response => {
                     //     console.log('logout response');
-                    // THIS IS ALL THAT HAPPENS AT LOGOUT
                         SecureStore.deleteItemAsync('user');
                         setUser(null);
                         setIsAuthenticated(false);
@@ -103,45 +60,6 @@ export const AuthProvider = ({children}) => {
                     //     console.error(error);
                     // });
                 },
-                // bearerLogin: (domain, token) => {
-                //     setIsLoading(true);
-                //     if (!token) {
-                //         console.log('token is empty');
-                //     }
-                //     if (!domain) {
-                //         console.log('domain is empty');
-                //     }
-                //     makeRequest({
-                //         domain: domain,
-                //         url: 'users/me',
-                //         method: 'GET',
-                //         isAuth: false, // if true this strips `api/v1` i think
-                //         headers: { 'Authorization': `Bearer ${token}` }
-                //     })
-                //         .then(response => {
-                //             console.log(response);
-                //             setIsAuthenticated(true);
-                //             SecureStore.setItemAsync('domain', domain);
-                //             const userResponse = {
-                //                 token: token,
-                //                 id: response.id,
-                //                 first_name: response.first_name,
-                //                 last_name: response.last_name,
-                //                 email: response.email,
-                //                 permissions: response.permissions,
-                //             }
-                //             setUser(userResponse);
-                //             SecureStore.setItemAsync('user', JSON.stringify(userResponse));
-                //             AsyncStorage.setItem('locale', response.locale || 'en-US')
-                //             setIsLoading(false);
-                //         })
-                //         .catch(error => {
-                //             setUser(null);
-                //             setIsAuthenticated(false);
-                //             console.error(error);
-                //             console.error(error.message);
-                //         });
-                // },
                 oAuthLogin: (domain, code, codeVerifier) => {
                    setIsLoading(true);
                    if (!code) {
@@ -151,25 +69,17 @@ export const AuthProvider = ({children}) => {
                    if (!domain) {
                        console.log('domain is empty');
                    }
+                   // set up url encoding
                     const params = new URLSearchParams();
                     params.append('grant_type', 'authorization_code');
                     params.append('client_id', '39');
                     params.append('code', code);
                     params.append('code_verifier', codeVerifier);
                     params.append('redirect_uri', 'com.grokability.snipeitmobile://home');
-                   // makeRequest({
-                   //     domain: domain,
-                   //     isAuth: true,
-                   //     url: '/oauth/token',
-                   //     method: 'POST',
-                   //     data: {
-                   //         client_id: '34',
-                   //         grant_type: 'authorization_code',
-                   //         code: code,
-                   //         code_verifier: codeVerifier,
-                   //         redirect_uri: 'com.grokability.snipeitmobile://home',
-                   //     }
-                   // })
+                    params.append('name', deviceName); // hm, this isn't working.
+                    console.log('params:', params);
+
+                    // make the actual token request
                     makeRequest({
                         headers: {
                             'Content-Type': 'application/x-www-form-urlencoded',
@@ -184,26 +94,23 @@ export const AuthProvider = ({children}) => {
                     .then(async response => {
                         const accessToken = response.access_token;
 
-                        // 1. Fetch user details using the new token
                         const userData = await makeRequest({
                             domain: domain,
-                            url: '/users/me', // or your specific 'me' endpoint
+                            url: '/users/me',
                             method: 'GET',
                             isAuth: false,
-                            headers: {'Authorization': `Bearer ${accessToken}`}
+                            headers: {'Authorization': `Bearer ${accessToken}`} // i think we might be able to use `state` now instead?
                         });
 
-                        // 2. Format the user object for your app state
                         const userResponse = {
                             token: accessToken,
+                            token_id: response.id,
                             id: userData.id,
                             first_name: userData.first_name,
                             last_name: userData.last_name,
                             email: userData.email,
-                            // add other fields as needed
                         };
 
-                        // 3. Persist and Update State
                         await SecureStore.setItemAsync('domain', domain);
                         await SecureStore.setItemAsync('user', JSON.stringify(userResponse));
 
@@ -215,9 +122,8 @@ export const AuthProvider = ({children}) => {
                            console.log('Server Error Data:', JSON.stringify(error.response.data, null, 2));
                        }
                        console.log(error);
-                       // setUser(null);
-                       // setIsAuthenticated(false);
-                       // console.error(error);
+                       setUser(null);
+                       setIsAuthenticated(false);
                    })
                    .finally(() => {
                        setIsLoading(false);
