c81af9dab6801d0b24d2eae166727b5f84098089
spencerrlongg <spencer@spencerlong.com>
2026-01-14 16:19:56 -0600

    lots of cleanup in button

--- components/BrowserLoginButton.jsx
+++ components/BrowserLoginButton.jsx
@@ -6,24 +6,12 @@ import {AuthContext, useAuth} from "@/context/AuthProvider";
 
 WebBrowser.maybeCompleteAuthSession();
 
-// Endpoint
-// const discovery = {
-//     authorizationEndpoint: 'https://snipe.ngrok.dev' + '/oauth/authorize',
-//     tokenEndpoint: 'https://snipe.ngrok.dev' + '/oauth/token', //maybe change this to /oauth/token/refresh ?? // probably not, that probably goes in AuthProvider
-//     revocationEndpoint: 'https://snipe.ngrok.dev' + '/oauth/revoke' //+ id, //will have to get the id back later
-//
-//     // revocationEndpoint: 'https://github.com/settings/connections/applications/<CLIENT_ID>',
-// };
-
 const BrowserLoginButton = ({ domain }) => {
     const { oAuthLogin } = useContext(AuthContext);
-    // Endpoint
     const discovery = {
         authorizationEndpoint: domain + '/oauth/authorize',
-        tokenEndpoint: domain + '/oauth/token', //maybe change this to /oauth/token/refresh ?? // probably not, that probably goes in AuthProvider
-        revocationEndpoint: domain + '/oauth/revoke' //+ id, //will have to get the id back later
-
-        // revocationEndpoint: 'https://github.com/settings/connections/applications/<CLIENT_ID>',
+        tokenEndpoint: domain + '/oauth/token',
+        revocationEndpoint: domain + '/oauth/revoke'
     };
 
     const redirectUri = makeRedirectUri({
@@ -36,17 +24,18 @@ const BrowserLoginButton = ({ domain }) => {
             usePKCE: true,
             responseType: 'code',
             clientId: '9999',
-            redirectUri: redirectUri
+            redirectUri: redirectUri,
+            extraParams: {
+                mobile: true,
+            }
         },
         discovery
     );
 
     const handleLogin = async () => {
-
-
         const result = await promptAsync()
 
-        console.log("manual prompt", result);
+        console.log(result);
 
         if (result?.type === 'success') {
             const { code } = result.params;
@@ -55,34 +44,10 @@ const BrowserLoginButton = ({ domain }) => {
         }
     }
 
-    // const redirectUri = makeRedirectUri({
-    //     scheme: 'com.grokability.snipeitmobile',
-    //     path: 'home',
-    // })
-
-
-
-    // console.log("generated redirect uri", redirectUri);
-
-
-
-
-    // useEffect(() => {
-    //     console.log(response);
-    //     if (response?.type === 'success') {
-    //         const { code } = response.params;
-    //         console.log(response)
-    //         oAuthLogin(domain, code, request.codeVerifier);
-    //     }
-    // }, [response]);
-
     return (
         <Button
             disabled={!request}
             title="Login"
-            // onPress={() => {
-            //     promptAsync();
-            // }}
             onPress={handleLogin}
         />
     );
