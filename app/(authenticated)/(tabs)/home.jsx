ca588409b21189aae3eb205305210e00fe91316c
spencerrlongg <spencer@spencerlong.com>
2026-04-08 16:43:39 -0500

    a poop commit

--- app/(authenticated)/(tabs)/home.jsx
+++ app/(authenticated)/(tabs)/home.jsx
@@ -78,6 +78,7 @@ export default function HomeScreen() {
                         loop
                     />
                     <Text style={styles.welcomeText}>{t('mobile.welcome')}</Text>
+                    <Text style={styles.welcomeText}>POOP</Text>
                     <Text style={styles.versionText}>
                         {t('mobile.version', { version: ExpoApplication.nativeApplicationVersion, build: ExpoApplication.nativeBuildVersion })}
                     </Text>
